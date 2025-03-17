19a7b8a34021fd3a90b69cbaba1ba2a992a91a1b
kimcarrington <165577489+kimcarrington@users.noreply.github.com>
2025-03-17 11:00:38 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# import libraries
+
+# handle xml file security
+
+# analyze file for key signature
+
+# transpose function
+
+# convert to midi function
+
+# main function to handle user interactions
+  # ask for file name
+  # print analysis results
+  # transpose
+  # return midi file
